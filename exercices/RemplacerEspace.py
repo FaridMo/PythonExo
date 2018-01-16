d6bc94e2aeb79f21eae943e5768dd6ad768a6722
faridmo <jurispenale@gmail.com>
2018-01-16 02:55:33 +0000

    remplacer espace

---
+++ exercices/RemplacerEspace.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+'''
+Created on 16 janv. 2018
+
+@author: __FaridMo__
+
+script permettant de remplacer l'espace se trouvant entre les mots en **
+'''
+
+a=input('Entrez votre phrase :')
+
+print(a.replace(' ', '**'))
